5b04eef7f5a3e1639940a92f532b0539db6a0ad9
Iason Sarantopoulos <iason@sarantop.com>
2018-07-10 17:53:48 +0300

    Fix the plot data script

--- rlrl-py/rlrl_py/utils/plot-data.py
+++ rlrl-py/rlrl_py/utils/plot-data.py
@@ -32,41 +32,56 @@ obs_rms_mean = []
 total_steps = []
 
 
-directory = '/tmp/openai-2018-07-10-15-18-28-893568'
+directory = '/tmp/openai-2018-07-10-15-46-29-849140'
 with open(os.path.join(directory, 'progress.csv'), 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-       reference_Q_mean = row["reference_Q_mean"]
-       reference_action_mean = row["reference_action_mean"]
-       reference_perturbed_action_mean = row["reference_perturbed_action_mean"]
-       reference_actor_Q_std = row["reference_actor_Q_std"]
-       param_noise_stddev = row["param_noise_stddev"]
-       reference_Q_std = row["reference_Q_std"]
-       reference_perturbed_action_std = row["reference_perturbed_action_std"]
-       total_epochs = row["total/epochs"]
-       rollout_episode_steps = row["rollout/episode_steps"]
-       rollout_Q_mean = row["rollout/Q_mean"]
-       reference_actor_Q_mean = row["reference_actor_Q_mean"]
-       total_episodes = row["total/episodes"]
-       rollout_actions_mean = row["rollout/actions_mean"]
-       rollout_episodes = row["rollout/episodes"]
-       total_duration = row["total/duration"]
-       train_loss_actor = row["train/loss_actor"]
-       total_steps_per_second = row["total/steps_per_second"]
-       obs_rms_std = row["obs_rms_std"]
-       train_loss_critic = row["train/loss_critic"]
-       rollout_actions_std = row["rollout/actions_std"]
-       rollout_return_history = row["rollout/return_history"]
-       reference_action_std = row["reference_action_std"]
-       rollout_return = row["rollout/return"]
-       train_param_noise_distance = row["train/param_noise_distance"]
-       obs_rms_mean = row["obs_rms_mean"]
-       total_steps = row["total/steps"]
-       
-       print(total_epochs)
+       reference_Q_mean.append(float(row["reference_Q_mean"]))
+       reference_action_mean.append(float(row["reference_action_mean"]))
+       reference_perturbed_action_mean.append(float(row["reference_perturbed_action_mean"]))
+       reference_actor_Q_std.append(float(row["reference_actor_Q_std"]))
+       param_noise_stddev.append(float(row["param_noise_stddev"]))
+       reference_Q_std.append(float(row["reference_Q_std"]))
+       reference_perturbed_action_std.append(float(row["reference_perturbed_action_std"]))
+       total_epochs.append(float(row["total/epochs"]))
+       rollout_episode_steps.append(float(row["rollout/episode_steps"]))
+       rollout_Q_mean.append(float(row["rollout/Q_mean"]))
+       reference_actor_Q_mean.append(float(row["reference_actor_Q_mean"]))
+       total_episodes.append(float(row["total/episodes"]))
+       rollout_actions_mean.append(float(row["rollout/actions_mean"]))
+       rollout_episodes.append(float(row["rollout/episodes"]))
+       total_duration.append(float(row["total/duration"]))
+       train_loss_actor.append(float(row["train/loss_actor"]))
+       total_steps_per_second.append(float(row["total/steps_per_second"]))
+       obs_rms_std.append(float(row["obs_rms_std"]))
+       train_loss_critic.append(float(row["train/loss_critic"]))
+       rollout_actions_std.append(float(row["rollout/actions_std"]))
+       rollout_return_history.append(float(row["rollout/return_history"]))
+       reference_action_std.append(float(row["reference_action_std"]))
+       rollout_return.append(float(row["rollout/return"]))
+       train_param_noise_distance.append(float(row["train/param_noise_distance"]))
+       obs_rms_mean.append(float(row["obs_rms_mean"]))
+       total_steps.append(float(row["total/steps"]))
 
-plt.plot(total_epochs, rollout_return)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out')
+rewards = []
+length = []
+time = []
+with open(os.path.join(directory, '0.monitor.csv'), 'r') as csvfile1:
+    reader = csv.DictReader(csvfile1)
+    for row in reader:
+       length.append(float(row["l"]))
+       time.append(float(row["t"]))
+       rewards.append(float(row["r"]))
+
+
+print(rollout_return)
+
+plt.plot(total_steps, rollout_return)
+plt.xlabel('Total Steps')
+plt.ylabel('Rollout Return')
+plt.show()
+
+plt.plot(time, rewards)
+plt.xlabel('Time')
+plt.ylabel('Rewards')
 plt.show()
