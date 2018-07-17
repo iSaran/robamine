624ad8ace9a0b0d37efb57e29f250a0e0f97cbb2
Iason Sarantopoulos <iason@sarantop.com>
2018-07-17 17:11:05 +0300

    Initial version of the new environment
    Changed the xml by adding site instead of geometry for the target

--- rlrl-py/rlrl_py/envs/bhand_slide_pillbox_2.py
+++ rlrl-py/rlrl_py/envs/bhand_slide_pillbox_2.py
@@ -8,7 +8,14 @@ import os
 import rlrl_py.utils as arl
 
 class BHandSlidePillbox2(robot_env.RobotEnv, utils.EzPickle):
-    def __init__(self):
+    def __init__(self, distance_threshold = 0.005):
+        """ Initializes a new BHand Slide Pillbox environment
+
+        Args:
+            distance_threshold (float): the threshold after which the goal is considered achieved
+        """
+
+        self.distance_threshold = distance_threshold
 
         # The path of the Mujoco XML model
         path = os.path.join(os.path.dirname(__file__),
@@ -31,6 +38,13 @@ class BHandSlidePillbox2(robot_env.RobotEnv, utils.EzPickle):
                 "bh_j32_joint": 3
                 }
 
+        self.sensor_name = {
+                'optoforce_1': [0, 1, 2],
+                'optoforce_2': [3, 4, 5]
+                }
+
+        self._max_episode_steps = 5000;  # used by HER
+
         # Initialize the Bhand joint configuration to a prespecified config
         self.init_config = {
                 "bh_j11_joint": 0.0,
@@ -39,81 +53,50 @@ class BHandSlidePillbox2(robot_env.RobotEnv, utils.EzPickle):
                 "bh_j32_joint": 0.0
                 }
 
-        self.sensor_name = {
-                'optoforce_1': [0, 1, 2],
-                'optoforce_2': [3, 4, 5]
-                }
+        #target_pillbox = self._sample_goal();
+        init_qpos = {
+           "bh_j11_joint": self.init_config["bh_j11_joint"],
+           "bh_j12_joint": self.init_config["bh_j12_joint"],
+           "bh_j22_joint": self.init_config["bh_j22_joint"],
+           "bh_j32_joint": self.init_config["bh_j32_joint"],
+           "bh_j21_joint": self.init_config["bh_j11_joint"],
+           "bh_j13_joint": 0.3333 * self.init_config["bh_j12_joint"],
+           "bh_j23_joint": 0.3333 * self.init_config["bh_j22_joint"],
+           "bh_j33_joint": 0.3333 * self.init_config["bh_j32_joint"],
+           "world_to_pillbox": [0.2, 0.92, 0.3725, 1, 0, 0, 0],
+           }
+        self.initial_obj_pos = [0.2, 0.92, 0.3725]
 
-        self._max_episode_steps = 5000;  # used by HER
 
         # Define the action space as the two forces on the wrist of the BHand
-        self.action_space = spaces.Box(low=np.array([-5, -5]),
-                                       high=np.array([5,  5]),
-                                       dtype=np.float32)
+        # self.action_space = spaces.Box(low=np.array([-5, -5]),
+        #                                high=np.array([5,  5]),
+        #                                dtype=np.float32)
 
-        # Define the observation space as the measured Wrench in BHand wrist
-        self.observation_space = spaces.Box(low=np.array([-1, -1, -1, -1, -1, -1]),
-                                            high=np.array([1, 1, 1, 1, 1, 1]),
-                                            dtype=np.float32)
+        # # Define the observation space as the measured Wrench in BHand wrist
+        # self.observation_space = spaces.Box(low=np.array([-1, -1, -1, -1, -1, -1]),
+        #                                     high=np.array([1, 1, 1, 1, 1, 1]),
+        #                                     dtype=np.float32)
+        #
+        # Set the initial joint positions of the hand
 
-        robot_env.RobotEnv.__init__(self, path, [], 10, 10)
+        robot_env.RobotEnv.__init__(self, path, init_qpos, n_actions=2, n_substeps=1)
         utils.EzPickle.__init__(self)
         self.bhand_joint_ids = self.get_joint_id()
 
-    def reset_model(self):
+    # GoalEnv methods
+    # ----------------------------
 
-        # Set the initial joint positions of the hand
-        for key in self.init_config:
-            self.init_qpos[self.sim.model.get_joint_qpos_addr(key)]  = self.init_config[key];
-        self.init_qpos[self.sim.model.get_joint_qpos_addr("bh_j21_joint")]  = self.init_config["bh_j11_joint"];
-        self.init_qpos[self.sim.model.get_joint_qpos_addr("bh_j13_joint")]  = 0.3333 * self.init_config["bh_j12_joint"];
-        self.init_qpos[self.sim.model.get_joint_qpos_addr("bh_j23_joint")]  = 0.3333 * self.init_config["bh_j22_joint"];
-        self.init_qpos[self.sim.model.get_joint_qpos_addr("bh_j33_joint")]  = 0.3333 * self.init_config["bh_j32_joint"];
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        distance = np.linalg.norm(achieved_goal - desired_goal)
+        return -(distance > self.distance_threshold).astype(np.float32)
 
-        self.set_state(self.init_qpos, self.init_qvel)
-        return self.get_obs()
+    # RobotEnv methods
+    # ----------------------------
 
-    def get_obs(self):
-        centroid = (self.sim.data.get_body_xpos('wam/bhand/finger_1/tip_link') + self.sim.data.get_body_xpos('wam/bhand/finger_2/tip_link')) / 2
-        dominant_point = centroid - self.sim.data.get_body_xpos('pillbox')
-        optoforce1 = np.linalg.norm(self.sim.data.sensordata[self.sensor_name["optoforce_1"]])
-        optoforce2 = np.linalg.norm(self.sim.data.sensordata[self.sensor_name["optoforce_2"]])
-
-        # Parse the joint positions
-        # hand_joints_pos = []
-        # for joint_name in self.bhand_joint_names:
-        #     if joint_name != "bh_wrist_joint":
-        #         hand_joints_pos.append(self.sim.data.get_joint_qpos(joint_name))
-
-        # Parse the pose of the object
-        #object_pose = self.sim.data.get_joint_qpos("world_to_pillbox")
-        #object_wrt_world = arl.get_homogeneous_transformation(object_pose)
-
-        # Read the wrist pose
-        #wrist_pose = self.sim.data.get_joint_qpos("bh_wrist_joint")
-        #wrist_wrt_world = arl.get_homogeneous_transformation(wrist_pose)
-        #wrist_wrt_object = np.linalg.inv(object_wrt_world) * wrist_wrt_world
-        #wrist_wrt_object_pose = arl.get_pose_from_homog(wrist_wrt_object)
-
-        target_pos = self.sim.data.get_body_xpos("pillbox") - self.sim.data.get_body_xpos("pillbox_target")
-        return np.concatenate((dominant_point, target_pos, [optoforce1], [optoforce2]))
-
-    def step(self, action):
-        done = False
-        obs = self.get_obs()
-        obs_her = {
-                'observation': np.concatenate((obs[0:3], [obs[3]], [obs[4]])),
-                'goal': obs[3:6]
-                }
-        reward = self.get_reward(obs)
-        time = self.do_simulation(action)
-        if self.terminal_state(obs):
-            reward = reward - 10000;
-            done = True
-        return obs_her, reward, done, {}
-
-    def do_simulation(self, action):
-        time = self.sim.data.time
+    def _set_action(self, action):
+        assert action.shape == (2,)
+        action = action.copy()  # ensure that we don't change the action outside of this scope
 
         # Set the bias to the applied generilized force in order to
         # implement a kind of gravity compensation in bhand.
@@ -132,9 +115,59 @@ class BHandSlidePillbox2(robot_env.RobotEnv, utils.EzPickle):
         for key in self.joint_ctrl_addr:
             self.sim.data.ctrl[self.joint_ctrl_addr[key]] = self.init_config[key]
 
-        # Move forward the simulation
-        self.sim.step()
-        return time
+    def _get_obs(self):
+        # Calculate the dominant point, i.e. the centroid of the dominant fingers
+        centroid = (self.sim.data.get_body_xpos('wam/bhand/finger_1/tip_link') + self.sim.data.get_body_xpos('wam/bhand/finger_2/tip_link')) / 2
+        dominant_point = centroid - self.sim.data.get_body_xpos('pillbox')
+
+        # Calculate the norms of the optoforce readings
+        optoforce1 = np.linalg.norm(self.sim.data.sensordata[self.sensor_name["optoforce_1"]])
+        optoforce2 = np.linalg.norm(self.sim.data.sensordata[self.sensor_name["optoforce_2"]])
+
+        # Concatenate all the above to the observation vector
+        obs = np.concatenate((dominant_point, [optoforce1], [optoforce2]))
+
+        # Read the object's position
+        obj_position = self.initial_obj_pos - self.sim.data.get_body_xpos('pillbox')
+
+        return {
+            'observation': obs.copy(),
+            'achieved_goal': np.squeeze(obj_position.copy()),
+            'desired_goal': np.squeeze(self.goal.copy())
+            }
+
+        return obs_dict
+
+    def _env_setup(self, initial_qpos):
+        for name, value in initial_qpos.items():
+            self.sim.data.set_joint_qpos(name, value)
+        self.sim.forward()
+
+    def _viewer_setup(self):
+        body_id = self.sim.model.body_name2id('pillbox')
+        lookat = self.sim.data.body_xpos[body_id]
+        for idx, value in enumerate(lookat):
+            self.viewer.cam.lookat[idx] = value
+        # Set the camera configuration (spherical coordinates)
+        self.viewer.cam.distance = 0.559478
+        self.viewer.cam.elevation = -21.928236
+        self.viewer.cam.azimuth = 9.089018
+
+    def _reset_sim(self):
+        self.sim.set_state(self.initial_state)
+        return True
+
+    def _render_callback(self):
+        index = self.sim.model.get_joint_qpos_addr("world_to_pillbox")
+        initial_world_to_pillbox_pos = self.initial_state.qpos[index[0]:index[0]+3]
+        world_to_pillbox_pos = self.sim.data.get_joint_qpos("world_to_pillbox")[0:3]
+        offset = world_to_pillbox_pos - initial_world_to_pillbox_pos
+        site_id = self.sim.model.site_name2id('target')
+        self.sim.model.site_pos[site_id] = self.goal - offset
+        self.sim.forward()
+
+
+
 
     def get_joint_id(self, joint_name = "all"):
         """
@@ -164,36 +197,47 @@ class BHandSlidePillbox2(robot_env.RobotEnv, utils.EzPickle):
                 output.append(index)
         return output
 
-    def viewer_setup(self):
-        # Set the camera configuration (spherical coordinates)
-        self.viewer.cam.distance = 0.559478
-        self.viewer.cam.elevation = -21.928236
-        self.viewer.cam.azimuth = 9.089018
-
-
-    def get_reward(self, observation):
-        dominant_point = observation[0:3]
-        goal_pos = observation[3:6]
-
-        reward1 = - 1000 * np.power(np.linalg.norm(goal_pos), 2)
-
-        if np.linalg.norm(goal_pos) < 0.001:
-            reward2 = 1000
-        else:
-            reward2 = 0
-
-        reward3= - 1000 * np.power(dominant_point[0], 2) - 1000 * np.power(dominant_point[1], 2)
-
-        optoforce1 = observation[6]
-        optoforce2 = observation[7]
-        reward4 = - 0.5 * (1 / optoforce1 + 1/optoforce2)
-
-        reward = reward1 + reward2 + reward3 + reward4
-        # print('reward1: ', reward1, 'reward2: ', reward2, 'reward3: ', reward3, 'reward4:', reward4)
-        return reward
 
     def terminal_state(self, observation):
         if np.linalg.norm(observation[0:3]) > 0.5:
             return True
 
         return False
+
+    def _sample_goal(self):
+        "Samples a goal for the object w.r.t the frame of the object."
+        mean = 0.035
+        dev = 0.01
+        sample_y = - np.random.normal(mean, dev, 1)
+        sampled_goal = [0, sample_y[0], 0]
+        return sampled_goal
+
+    def _is_success(self, achieved_goal, desired_goal):
+        epsilon = 0.005
+        if np.linalg.norm(achieved_goal - desired_goal) < epsilon:
+            return True
+        else:
+            return False
+
+    def get_initial_state(self, model_path):
+        # Load local copy of the Mujoco models to have access to joint names and ids
+
+        if model_path.startswith('/'):
+            path = model_path
+        else:
+            path = os.path.join(os.path.dirname(__file__), 'assets', model_path)
+        if not os.path.exists(path):
+            raise IOError('File {} does not exist'.format(path))
+
+        model = load_model_from_path(path)
+        sim = MjSim(model)
+        viewer = None
+        sim.forward()
+
+        init_qpos = sim.data.qpos
+        for key in self.init_config:
+            init_qpos[sim.model.get_joint_qpos_addr(key)]  = self.init_config[key];
+
+        object_pos = sim.data.get_body_xpos('pillbox')
+
+        return init_qpos, object_pos
