dcea317c85f2fbefc156fc7d2e4bfe4aa5599eaa
Iason Sarantopoulos <iason@sarantop.com>
2018-07-27 15:57:19 +0300

    Create wrappers for baselines in order to keep the baselines code unmodified

---
+++ rlrl-py/rlrl_py/scripts/baselines_wrappers/her/play.py
@@ -0,0 +1,5 @@
+import baselines.her.experiment.play as main
+import rlrl_py
+
+if __name__ == '__main__':
+    main.main()
---
+++ rlrl-py/rlrl_py/scripts/baselines_wrappers/her/train.py
@@ -0,0 +1,5 @@
+import baselines.her.experiment.train as main
+import rlrl_py
+
+if __name__ == '__main__':
+    main.main()
