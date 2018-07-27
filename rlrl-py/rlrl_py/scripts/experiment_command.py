01bb3137ea3568eaa3a6806c8a8ba70a1989959e
Iason Sarantopoulos <iason@sarantop.com>
2018-07-27 16:21:47 +0300

    Flag experiments as completed

--- rlrl-py/rlrl_py/scripts/experiment_command.py
+++ rlrl-py/rlrl_py/scripts/experiment_command.py
@@ -7,22 +7,25 @@ def get_yml(path, name):
 
 def get_exp_names_commands(yml):
     exp_names = []
+    names_completed = []
     commands = {}
 
     for i in yml:
         command = "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/nvidia-384/libGL.so python train.py"
         if i["name"] not in exp_names:
             exp_names.append(i["name"])
+            if i["completed"]:
+                names_completed.append(i["name"])
             for param in i["params"]:
                 for key in param:
                   command = command + " --" + key + "=" + str(param[key])
             commands[i["name"]] = command
         else:
             raise RuntimeError("Names of the experiments should be unique")
-    return exp_names, commands
+    return exp_names, names_completed, commands
 
 
 if __name__ == '__main__':
     yml = get_yml("./", "experiments")
-    names, commands = get_exp_names_commands(yml)
+    names, completed, commands = get_exp_names_commands(yml)
     print(commands['centroid-goal'])
