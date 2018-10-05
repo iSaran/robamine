5615c9dbfceffc0bfdddfef489e9db0cbb644674
Iason Sarantopoulos <iason@sarantop.com>
2018-10-05 17:59:10 +0300

    Remove extra file in examples

--- rlrl-py/examples/ddpg-sphere-reacher.py
+++
@@ -1,8 +0,0 @@
-from rlrl_py.algo.ddpg import DDPG
-
-import tensorflow as tf
-
-
-if __name__ == '__main__':
-    with tf.Session() as sess:
-        agent = DDPG(sess, 'Pendulum-v0', exploration_noise_sigma=0.01, log_dir='/home/iason/rlrl_logs').train(n_episodes=1000, n_epochs=100, render=False, episodes_to_evaluate=0)
