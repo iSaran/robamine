14ca4a845f55198e3f6b7be3c12dbdb7d88be7a0
Iason Sarantopoulos <iason@sarantop.com>
2018-10-05 18:16:30 +0300

    Minor change to the example

--- examples/train-ddpg-pendulum.py
+++ examples/train-ddpg-pendulum.py
@@ -5,4 +5,4 @@ import tensorflow as tf
 
 if __name__ == '__main__':
     with tf.Session() as sess:
-        agent = DDPG(sess, 'Pendulum-v0', exploration_noise_sigma=0.01, log_dir='/home/iason/rlrl_logs/ddpg-pendulum').train(n_episodes=1000, n_epochs=10, render=False, episodes_to_evaluate=0)
+        agent = DDPG(sess, 'Pendulum-v0', exploration_noise_sigma=0.01, log_dir='/home/iason/rlrl_logs/ddpg-pendulum').train(n_episodes=1000, n_epochs=100, render=False, episodes_to_evaluate=0)
