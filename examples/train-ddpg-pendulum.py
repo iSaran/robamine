934c363b509d8b64a6090d0b9d1bbddc842c3a19
Iason Sarantopoulos <iason@sarantop.com>
2018-11-05 14:45:47 +0200

    Fix example to train DDPG with pendulum

--- examples/train-ddpg-pendulum.py
+++ examples/train-ddpg-pendulum.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
 
     with tf.Session() as sess:
         agent = rb.algo.ddpg.DDPG(sess, 'Pendulum-v0', exploration_noise_sigma=0.2)
-        agent.train(n_episodes=2, episode_batch_size=1, render=True, episodes_to_evaluate=1, render_eval = True)
+        agent.train(n_episodes=1000, episode_batch_size=25, render=False, episodes_to_evaluate=10, render_eval=False)
