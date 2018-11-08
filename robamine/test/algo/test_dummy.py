b886edfd7b00aacb7c06eaf129ea7a8f872e2883
Iason Sarantopoulos <iason@sarantop.com>
2018-11-08 19:31:44 +0200

    Update dummy unittest

--- robamine/test/algo/test_dummy.py
+++ robamine/test/algo/test_dummy.py
@@ -4,25 +4,24 @@ import tensorflow as tf
 import numpy as np
 import os
 
-from robamine.algo.dummy import Dummy
-from robamine.algo.util import Plotter
+import robamine as rm
 
 class TestAgent(unittest.TestCase):
     def test_construct(self):
-        with tf.Session() as sess:
-            agent = Dummy(sess, 'Pendulum-v0', random_seed=999, console = False)
-            agent.train(n_episodes=50, episode_batch_size=5, episodes_to_evaluate=10)
+        rm.rb_logging.init(console_level=rm.rb_logging.ERROR, file_level=rm.rb_logging.ERROR)
+        world = rm.World.create(rm.DummyParams(), 'Pendulum-v0')
+        world.train_and_eval(n_episodes_to_train=50, n_episodes_to_evaluate=10, evaluate_every=5)
+        world.plot(10)
+        streams = ['train', 'batch_train', 'eval', 'batch_eval']
+        pl = rm.Plotter(world.log_dir, streams)
+        pl_2 = rm.Plotter(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'robamine_logs_2018.11.08.19.26.24.35621/Dummy_Pendulum-v0'), streams)
 
-            streams = ['train_episode', 'train_batch', 'eval_episode', 'eval_batch']
-            pl = Plotter(agent.logger.log_path, streams)
-            pl_2 = Plotter(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'robamine_logger_Dummy_Pendulum-v0_2018.10.30.16.54.48.378300'), streams)
-
-            for stream in streams:
-                x, y = pl.extract_data(stream)
-                x_d, y_d = pl_2.extract_data(stream)
-                for y_var in y_d:
-                    error = np.array(y_d[y_var]) - np.array(y[y_var])
-                    self.assertEqual(np.max(error), 0.0)
+        for stream in streams:
+            x, y, _, _= pl.extract_data(stream)
+            x_d, y_d, _, _ = pl_2.extract_data(stream)
+            for y_var in y_d:
+                error = np.array(y_d[y_var]) - np.array(y[y_var])
+                self.assertEqual(np.max(error), 0.0)
 
 if __name__ == '__main__':
     unittest.main()
