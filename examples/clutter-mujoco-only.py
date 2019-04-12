fed9a21bb417e3a64e67c85e8072c687ad8a1ae7
Iason Sarantopoulos <iason@sarantop.com>
2019-04-12 15:27:33 +0300

    Offscreen render does not work without run first window once

--- examples/clutter-mujoco-only.py
+++ examples/clutter-mujoco-only.py
@@ -2,6 +2,8 @@ import argparse
 from mujoco_py import load_model_from_path, MjSim
 import os
 import cv2
+import glfw
+import OpenGL.GL as gl
 
 if __name__ == '__main__':
     path = os.path.join(os.path.dirname(__file__),
@@ -9,11 +11,26 @@ if __name__ == '__main__':
 
     model = load_model_from_path(path)
     sim = MjSim(model)
-    for i in range (3000):
+
+    if sim._render_context_window is None:
+        print('window context is none')
+    sim.render(mode='window')
+    if sim._render_context_window is None:
+        print('window context is None again')
+    for i in range(3000):
 
         if i == 500 or i ==1500 or i==2000:
+            if sim._render_context_offscreen is None:
+                print('context offscreen is none')
             rgb, depth = sim.render(1920, 1080, depth=True, camera_name='xtion', mode='offscreen')
-            cv2.imwrite("/home/iason/Desktop/fds/obs.png", rgb)
+            if sim._render_context_offscreen is None:
+                print('context offscreen is none again')
 
-        sim.render(mode='window')
+            cv2.imwrite("/home/iason/Desktop/fds/obs_" + str(i) +".png", rgb)
+        # if i % 1 == 0:
+        #     sim.render(mode='window')
+            #cv2.imwrite("/home/iason/Desktop/fds/obs_" + str(i) +".png", rgb)
+        #else:
+        #    sim.render(mode='window', device_id=0)
+
         sim.step()
