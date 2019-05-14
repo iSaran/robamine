2d77b36b8ea3e55e06a05fd76ccc57d7a430f366
Iason Sarantopoulos <iason@sarantop.com>
2019-05-14 11:01:04 +0300

    Use mujoco py set state

--- examples/run-mujoco-sim.py
+++ examples/run-mujoco-sim.py
@@ -1,4 +1,5 @@
 from mujoco_py import load_model_from_path, MjSim, MjViewer, MjRenderContextOffscreen
+import mujoco_py
 import os
 import math
 import time
@@ -14,19 +15,31 @@ if __name__ == '__main__':
 
     joint_ids = [6, 7, 8, 9, 10, 11, 13, 16, 18]
 
+    sim.step()
+    init_qpos = sim.data.qpos.ravel().copy()
+    init_qvel = sim.data.qvel.ravel().copy()
+
     for i in range(10000):
         for index in joint_ids:
             sim.data.qfrc_applied[index] = sim.data.qfrc_bias[index]
 
         j = sim.model.get_joint_qpos_addr('bh_wrist_joint')
-        sim.data.qpos[j[0]] = 0.2
-        sim.data.qpos[j[0]+1] = 0.54
-        sim.data.qpos[j[0]+2] = 0.31
+        init_qpos[j[0]] = 0.2
+        init_qpos[j[0]+1] = 0.54
+        init_qpos[j[0]+2] = 0.31
 
-        # j = sim.model.get_joint_qpos_addr('bh_j11_joint')
-        # sim.data.qpos[j] = 0.2
+        j = sim.model.get_joint_qpos_addr('bh_j11_joint')
+        init_qpos[j] = 0.2
         j = sim.model.get_joint_qpos_addr('bh_j21_joint')
+        init_qpos[j] = 0.2
 
-        sim.data.qpos[j] = 0.2
+        j = sim.model.get_joint_qpos_addr('bh_j22_joint')
+        init_qpos[j] = 1
+
+        old_state = sim.get_state()
+        new_state = mujoco_py.MjSimState(old_state.time, init_qpos, init_qvel,
+                                         old_state.act, old_state.udd_state)
+        sim.set_state(new_state)
+
         sim.step()
         viewer.render()
