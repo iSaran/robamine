726c9230540cbf28bd7c09ef8bdd66635d190a72
Kiatos <kiatosm@iti.gr>
2019-05-27 16:52:57 +0300

    faster implemenation of get_obs

--- robamine/utils/cv_tools.py
+++ robamine/utils/cv_tools.py
@@ -34,6 +34,25 @@ def depth_to_point_cloud(depth, camera_intrinsics):
 
     return np.asarray(point_cloud)
 
+def point_cloud_vectorize(depth, camera_intrinsics):
+    fx = camera_intrinsics[0]
+    fy = camera_intrinsics[1]
+    cx = camera_intrinsics[2]
+    cy = camera_intrinsics[3]
+
+    rows, cols = depth.shape
+    c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+    valid = (depth > 0)
+
+    z = np.where(valid, -depth, 0)
+    x = np.where(valid, z * (c - cx) / fx, 0)
+    y = np.where(valid, z * (r - cy) / fy, 0)
+    pcd = np.dstack((x, y, z))
+
+    return pcd.reshape(-1, 3)
+    return pcd
+
+
 
 def transform_point_cloud(point_cloud, affine_transformation):
     """
@@ -49,6 +68,9 @@ def transform_point_cloud(point_cloud, affine_transformation):
     # Transform cloud
     for i in range(point_cloud.shape[0]):
         point_cloud[i] = np.matmul(affine_transformation, point_cloud[i])
+
+    # point_cloud = np.matmul(affine_transformation, point_cloud.T)
+    # point_cloud = point_cloud.T
 
     # Convert homogeneous to cartesian
     w = point_cloud[:, 3]
@@ -68,11 +90,15 @@ def gl2cv(depth, z_near, z_far):
     h, w = depth.shape
     linear_depth = np.zeros((h, w), dtype=np.float32)
 
-    for x in range(0, w):
-        for y in range(0, h):
-            if depth[y][x] != 1:
-                linear_depth[y][x] = 2 * z_far * z_near / (z_far + z_near - (z_far - z_near) * (2 * depth[y][x] - 1))
-
+    # for x in range(0, w):
+    #     for y in range(0, h):
+    #         if depth[y][x] != 1:
+    #             linear_depth[y][x] = 2 * z_far * z_near / (z_far + z_near - (z_far - z_near) * (2 * depth[y][x] - 1))
+    #
+    # linear_depth = np.flip(linear_depth, axis=1)
+    # return np.flip(linear_depth, axis=0)
+    valid = np.where(depth!=1.0)
+    linear_depth[valid] = 2 * z_far * z_near / (z_far + z_near - (z_far - z_near) * (2 * depth[valid] - 1))
     linear_depth = np.flip(linear_depth, axis=1)
     return np.flip(linear_depth, axis=0)
 
@@ -125,6 +151,7 @@ def generate_height_map(point_cloud, shape=(100, 100), grid_step=0.0025, plot=Fa
         cv_height = np.zeros((height, width), dtype=np.float32)
         min_height = np.min(height_grid)
         max_height = np.max(height_grid)
+        print(max_height)
         for i in range(0, width):
             for j in range(0, height):
                 cv_height[i][j] = (height_grid[i][j] - min_height) / (max_height - min_height)
