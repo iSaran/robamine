2a8764817c56d74f64b5e49bf9fddd90cffe02ed
Iason Sarantopoulos <iason@sarantop.com>
2019-05-29 10:40:14 +0300

    Train for more episodes and do not record debug in console log
    For reducing the size

--- examples/train.py
+++ examples/train.py
@@ -4,7 +4,7 @@ import robamine as rm
 
 
 if __name__ == '__main__':
-    rm.rb_logging.init('/tmp/robamine_logs/')
+    rm.rb_logging.init(directory='/tmp/robamine_logs/', file_level=logging.INFO)
     logger = logging.getLogger('robamine')
     world = rm.World(rm.DDPGParams(exploration_noise='Normal'), 'Clutter-v0')
-    world.train(n_episodes=500, print_progress_every=1, save_every=10)
+    world.train(n_episodes=5000, print_progress_every=10, save_every=10)
