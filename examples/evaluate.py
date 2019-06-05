2839c30a5e77c705eadb031eab1b570e369eb9a5
Kiatos <kiatosm@iti.gr>
2019-06-05 10:25:45 +0300

    push top right

--- examples/evaluate.py
+++ examples/evaluate.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     rm.rb_logging.init('/tmp/robamine_logs/')
     logger = logging.getLogger('robamine')
 
-    loaded_agent = rm.DDPG.load('/tmp/robamine_logs/robamine_logs_2019.06.03.13.09.30.384616/DDPG_Clutter-v0/model.pkl')
+    loaded_agent = rm.DDPG.load('/tmp/robamine_logs/robamine_logs_2019.06.04.14.31.48.866673/DDPG_Clutter-v0/model.pkl')
 
     world = rm.World(loaded_agent, 'Clutter-v0')
     world.evaluate(n_episodes=100, print_progress_every=1, render=True)
