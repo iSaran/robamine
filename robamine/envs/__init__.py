4436c621566c35fdecc2602fd27ff2169c3bccec
Robot PC <kuka.lwr@gmail.com>
2019-06-24 15:00:12 +0300

    Remove mujoco for running in Robot PC

--- robamine/envs/__init__.py
+++ robamine/envs/__init__.py
@@ -1,10 +1,10 @@
 # from robamine.envs.floating_bhand import FloatingBHand
-from robamine.envs.bhand_slide_pillbox import BHandSlidePillbox
-from robamine.envs.bhand_slide_pillbox_2 import BHandSlidePillbox2
-from robamine.envs.bhand_slide_pillbox_3 import BHandSlidePillbox3
-from robamine.envs.finger_slide import FingerSlide
-from robamine.envs.sphere_position import SpherePosition
-from robamine.envs.sphere_reacher import SphereReacher
-from robamine.envs.sphere_reacher_incremental import SphereReacherIncremental
+# from robamine.envs.bhand_slide_pillbox import BHandSlidePillbox
+# from robamine.envs.bhand_slide_pillbox_2 import BHandSlidePillbox2
+# from robamine.envs.bhand_slide_pillbox_3 import BHandSlidePillbox3
+# from robamine.envs.finger_slide import FingerSlide
+# from robamine.envs.sphere_position import SpherePosition
+# from robamine.envs.sphere_reacher import SphereReacher
+# from robamine.envs.sphere_reacher_incremental import SphereReacherIncremental
 from robamine.envs.lwr_ros import LWRROS
 # from robamine.envs.clutter import Clutter
