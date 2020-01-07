40f7b47eb1d80d103fa079c141207d0fb9cd8ed3
Iason Sarantopoulos <iason@sarantop.com>
2020-01-07 18:34:27 +0200

    Testing changes of split dynamics model

--- robamine/algo/splitdynamicsmodelposelstm.py
+++ robamine/algo/splitdynamicsmodelposelstm.py
@@ -1,6 +1,6 @@
 """
-Spit Dynamics Model Pose
-========================
+Spit Dynamics Model Pose LSTM
+=============================
 """
 
 import torch
@@ -11,11 +11,17 @@ import math
 import numpy as np
 
 import logging
-logger = logging.getLogger('robamine.algo.splitdynamicsmodelpose')
+logger = logging.getLogger('robamine.algo.splitdynamicsmodelposelstm')
 
 class SplitDynamicsModelPoseLSTM(LSTMSplitDynamicsModel):
     def __init__(self, params, inputs=4, outputs=3, name='SplitDynamicsModelPose'):
         super().__init__(params=params, inputs=inputs, outputs=outputs, name=name)
+
+    def _clutter2array(self, extra_data):
+        inputs = np.concatenate((extra_data[0], extra_data[1]), axis=1)
+        inputs = np.delete(inputs, 2, axis=1)
+        inputs = np.delete(inputs, -1, axis=1)
+        return inputs
 
     def load_dataset(self, env_data):
         """ Transforms the EnvData x to a Dataset """
@@ -30,29 +36,115 @@ class SplitDynamicsModelPoseLSTM(LSTMSplitDynamicsModel):
         for i in range(len(env_data.info['extra_data'])):
             # Extra data 0, 1 is the pushing distance and the angle of the action in rads
 
-            j = int(env_data.transitions[i].action - np.floor(env_data.transitions[i].action / self.nr_substates) * self.nr_substates)
-            state_split = np.split(env_data.transitions[i].state, self.nr_substates)
-            push_distance = state_split[j][-2]
-            bounding_box_1 = state_split[j][-7]
-            bounding_box_2 = state_split[j][-8]
-            angle = j * 2 * math.pi / (self.nr_primitives * self.nr_substates / self.nr_primitives)
-            pose_input = np.array([push_distance, bounding_box_1, bounding_box_2, angle])
             primitive = int(np.floor(env_data.transitions[i].action / self.nr_substates))
 
+            # print(env_data.info['extra_data'][i]['push_forces_vel'])
+            inputs = self._clutter2array(env_data.info['extra_data'][i]['push_forces_vel'])
             # Extra data in 2 has a np array with the displacement in (x, y, theta_around_z)
-            dataset[primitive].append(Datapoint(x = pose_input, y = env_data.info['extra_data'][i][2]))
+            dataset[primitive].append(Datapoint(x = inputs, y = env_data.info['extra_data'][i]['displacement'][2]))
 
+        # print('shape x:', dataset[0][0].x.shape)
+        # print('shape y:', dataset[0][0].y.shape)
         for i in range(self.nr_primitives):
             self.dynamics_models[i].load_dataset(dataset[i])
 
     def predict(self, state, action):
         # Assuming that state is the pushing distance and action the index of
         # the action
+        inputs = self._clutter2array(state)
         primitive = int(np.floor(action / self.nr_substates))
         if primitive == 1:
             return np.array([0, 0, 0])
-        substate_index = int(action - np.floor(action / self.nr_substates) * self.nr_substates)
-        angle = substate_index * 2 * math.pi / (self.nr_primitives * self.nr_substates / self.nr_primitives)
-        mystate = np.array([state[0], state[1], state[2], angle])
-        prediction = super().predict(mystate, action)
+        prediction = super().predict(inputs, action)
         return prediction
+
+    def learn(self):
+        self.iterations += 1
+
+        for i in range(self.nr_primitives):
+            if i!=1:
+                self.dynamics_models[i].learn()
+            self.info['train']['loss_' + str(i)] = \
+                self.dynamics_models[i].info['train']['loss']
+            self.info['test']['loss_' + str(i)] = \
+                self.dynamics_models[i].info['test']['loss']
+
+    def filter_datapoint(datapoint, epsilon=1e-8, filter=0.9, outliers_cutoff=3.8, plot=False):
+        '''Assume datapoint is output of _clutter2array'''
+        
+        import matplotlib.pyplot as plt
+        pos_measurements = datapoint[0]
+        force_measurements = datapoint[1]
+
+        # Calculate force direction
+        # -------------------------
+        f = force_measurements[:, :2].copy()
+        f = np.nan_to_num(f / norm(f, axis=1).reshape(-1, 1))
+        f_norm = np.linalg.norm(f, axis=1)
+
+        # Find start and end of the contacts
+        first = f_norm[0]
+        for i in range(f_norm.shape[0]):
+            if abs(f_norm[i] - first) > epsilon:
+                break
+        start_contact = i
+
+        first = f_norm[-1]
+        for i in reversed(range(f_norm.shape[0])):
+            if abs(f_norm[i] - first) > epsilon:
+                break;
+        end_contact = i
+
+        # No contact with the target detected
+        if start_contact > end_contact:
+            return np.zeros(2), 0.0
+
+        f = f[start_contact:end_contact, :]
+
+        if plot:
+            fig, axs = plt.subplots(2,2)
+            axs[0][0].plot(f)
+            plt.title('Force')
+            axs[0][1].plot(np.linalg.norm(f, axis=1))
+            plt.title('norm')
+
+        f[:,0] = filter_signal(signal=f[:,0], filter=filter, outliers_cutoff=outliers_cutoff)
+        f[:,1] = filter_signal(signal=f[:,1], filter=filter, outliers_cutoff=outliers_cutoff)
+        f = np.nan_to_num(f / norm(f, axis=1).reshape(-1, 1))
+
+        if plot:
+            axs[1][0].plot(f)
+            plt.title('Filtered force')
+            axs[1][1].plot(np.linalg.norm(f, axis=1))
+            plt.title('norm')
+            plt.show()
+
+        # Velocity direction
+        p = pos_measurements[start_contact:end_contact, :2].copy()
+        p_dot = np.concatenate((np.zeros((1, 2)), np.diff(p, axis=0)))
+        p_dot_norm = norm(p_dot, axis=1).reshape(-1, 1)
+        p_dot_normalized = np.nan_to_num(p_dot / p_dot_norm)
+
+        if plot:
+            fig, axs = plt.subplots(2)
+            axs[0].plot(p_dot_normalized)
+            axs[0].set_title('p_dot normalized')
+            axs[1].plot(p_dot)
+            axs[1].set_title('p_dot')
+            plt.legend(['x', 'y'])
+            plt.show()
+
+        perpedicular_to_p_dot_normalized = np.zeros(p_dot_normalized.shape)
+        for i in range(p_dot_normalized.shape[0]):
+            perpedicular_to_p_dot_normalized[i, :] = np.cross(np.append(p_dot_normalized[i, :], 0), np.array([0, 0, 1]))[:2]
+
+        inner = np.diag(np.matmul(-p_dot_normalized, np.transpose(f))).copy()
+        inner_perpedicular = np.diag(np.matmul(perpedicular_to_p_dot_normalized, np.transpose(f))).copy()
+        if plot:
+            plt.plot(inner)
+            plt.title('inner product')
+            plt.show()
+
+        # concat
+
+        return signal
---
+++ robamine/test/algo/test_splitdynamicsmodelposelstm.py
@@ -0,0 +1,117 @@
+import unittest
+from robamine.algo.splitdynamicsmodelposelstm import SplitDynamicsModelPoseLSTM
+from robamine.algo.util import EnvData, Transition
+import os
+import numpy.testing as np_test
+import numpy as np
+import gym
+import torch
+
+class TestAgent(unittest.TestCase):
+    def test_init(self):
+        params = {
+            'action_dim': 16,
+            'state_dim': 2120,
+            'batch_size': [64, 64],
+            'device': 'cpu',
+            'hidden_units': [20, 20],
+            'learning_rate': [0.001, 0.001],
+            'loss': ['mse', 'mse'],
+            'n_epochs': [1000, 1000],
+            'n_layers': [2, 2]
+        }
+        model = SplitDynamicsModelPoseLSTM(params)
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saved_model.pkl')
+        model.save(path)
+
+        model_loaded = SplitDynamicsModelPoseLSTM.load(path)
+
+        self.assertEqual(model.inputs, model_loaded.inputs)
+        self.assertEqual(model.outputs, model_loaded.outputs)
+
+    def test_learn(self):
+        # Tested in the integrated test with Clutter Env
+        pass
+
+    def test_predict(self):
+        # Tested in the integrated test with Clutter Env
+        pass
+
+    def test_filter_datapoint(self):
+        # path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        #                     'test_splitdynamicsmodelposelstm.env')
+        # env_data = EnvData.load(path)
+        pass
+
+
+class TestIntegrationWithClutterEnv(unittest.TestCase):
+    def test_learn(self):
+
+        # Create env and compile EnvData for pushes without obstacles
+        # -----------------------------------------------------------
+
+        env_params = {
+        'name': 'Clutter-v0',
+        'params': {
+          'discrete': True,
+          'nr_of_actions': 16,  # u x w
+          'render': False,
+          'nr_of_obstacles': [0, 0],
+          'target_probability_box': 1.0,
+          'target_height_range': [0.01, 0.01],
+          'obstacle_probability_box': 1.0,
+          'obstacle_height_range': [0.005, 0.005],
+          'push_distance': [0.15, 0.15],
+          'split': True,
+          'extra_primitive': False,
+          'all_equal_height_prob': 0.0,
+          'finger_size': [0.005, 0.005]
+          }
+        }
+        env = gym.make(env_params['name'], params=env_params['params'])
+        env.seed(0)
+        env_data = EnvData(['extra_data'])
+        action = [0, 1, 2, 8, 8]
+        for i in range(5):
+            state = env.reset()
+            next_state, reward, done, info = env.step(action[i])
+            transition = Transition(state, action[i], reward, next_state, done)
+            env_data.transitions.append(transition)
+            if 'extra_data' in info:
+                env_data.info['extra_data'].append(info['extra_data'])
+
+
+        # Create model, load dataset from clutter and learn for one epoch
+        # ---------------------------------------------------------------
+
+        params = {
+            'action_dim': 16,
+            'state_dim': 2120,
+            'batch_size': [64, 64],
+            'device': 'cpu',
+            'hidden_units': [20, 20],
+            'learning_rate': [0.001, 0.001],
+            'loss': ['mse', 'mse'],
+            'n_epochs': [1000, 1000],
+            'n_layers': [2, 2]
+        }
+        torch.manual_seed(0)
+        model = SplitDynamicsModelPoseLSTM(params)
+        model.load_dataset(env_data)
+        model.seed(0)
+        model.learn()
+
+        # Test predictions of the learned model
+        # -------------------------------------
+        action = 2
+        env.reset()
+        next_state, reward, done, info = env.step(action)
+        prediction = model.predict(info['extra_data']['push_forces_vel'], action)
+        np.testing.assert_equal(prediction, np.array([0.040626440197229385, 0.03152748942375183, 0.11238543689250946]))
+
+        prediction = model.predict(info['extra_data']['push_forces_vel'], 8)
+        np.testing.assert_equal(prediction, np.array([0.0, 0.0, 0.0]))
+
+if __name__ == '__main__':
+    unittest.main()
+# Datapoint[x= [0.06328722 1.57079633], y= [ 0.00518818  0.05335347 -0.03774902]]
