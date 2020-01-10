bc8a5ed00fa53691ad215a62afbc18c1e76bf7ab
Iason Sarantopoulos <iason@sarantop.com>
2020-01-10 12:03:45 +0200

    Add minimal script for running training

---
+++ experiments/2020.01.19_ddpg/test_ddpg.py
@@ -0,0 +1,16 @@
+from robamine.algo.core import TrainWorld
+from robamine.algo.dqn import DQN
+from robamine import rb_logging
+import logging
+import yaml
+
+def run():
+    with open('params.yml', 'r') as stream:
+        params = yaml.safe_load(stream)
+
+    rb_logging.init(directory=params['world']['logging_dir'], friendly_name=params['world']['friendly_name'], file_level=logging.INFO)
+    trainer = TrainWorld(agent=params['agent'], env='Pendulum-v0', params=params['world']['params'])
+    trainer.run()
+
+if __name__ == '__main__':
+    run()
