9f0910a0d8bcdf6c07cc5b3242f02815e38bbb14
Iason Sarantopoulos <iason@sarantop.com>
2020-02-21 19:53:11 +0200

    Add a class implementing a line segment
    Useful for finding cross section of line segments etc

--- robamine/utils/math.py
+++ robamine/utils/math.py
@@ -2,6 +2,7 @@
 from math import exp
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 def sigmoid(x, a=1, b=1, c=0, d=0):
     return a / (1 + exp(-b * x + c)) + d;
@@ -64,6 +65,86 @@ def filter_signal(signal, filter=0.9, outliers_cutoff=None):
 
     return signal_
 
+class LineSegment2D:
+    def __init__(self, p1, p2):
+        self.p1 = p1.copy()
+        self.p2 = p2.copy()
+
+    def get_point(self, lambd):
+        assert lambd >=0 and lambd <=1
+        return (1 - lambd) * self.p1 + lambd * self.p2
+
+    def get_lambda(self, p3):
+        lambd = (p3[0] - self.p1[0]) / (self.p2[0] - self.p1[0])
+        lambd_2 = (p3[1] - self.p1[1]) / (self.p2[1] - self.p1[1])
+        if abs(lambd - lambd_2) > 1e-5:
+            return None
+        return lambd
+
+    def get_intersection_point(self, line_segment, belong_self=True, belong_second=True):
+        '''See wikipedia https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line'''
+        x1 = self.p1[0]
+        y1 = self.p1[1]
+        x2 = self.p2[0]
+        y2 = self.p2[1]
+
+        x3 = line_segment.p1[0]
+        y3 = line_segment.p1[1]
+        x4 = line_segment.p2[0]
+        y4 = line_segment.p2[1]
+
+        if abs((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)) < 1e-10:
+            return None
+
+        t =  ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / \
+             ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+        u = - ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / \
+              ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+
+        p = np.array([x1 + t * (x2 - x1), y1 + t * (y2 - y1)])
+
+        if belong_self and belong_second:
+            if t >=0 and t <= 1 and u >= 0 and u <= 1:
+                return p
+            else:
+                return None
+        elif (belong_self and t >=0 and t <= 1) or (belong_second and u >= 0 and u <= 1):
+            return p
+        elif not belong_self and not belong_second:
+            return p
+        return None
+
+    def norm(self):
+        return np.linalg.norm(self.p1 - self.p2)
+
+    def __str__(self):
+        return self.p1.__str__() + self.p2.__str__()
+
+
+    @staticmethod
+    def plot_line_segments(line_segments, points=[]):
+        color = iter(plt.cm.rainbow(np.linspace(0, 1, len(line_segments) + len(points))))
+        lines = []
+        i = 0
+        for line_segment in line_segments:
+            c = next(color)
+            plt.plot(line_segment.p1[0], line_segment.p1[1], color=c, marker='o')
+            plt.plot(line_segment.p2[0], line_segment.p2[1], color=c, marker='.')
+            plt.plot([line_segment.p1[0], line_segment.p2[0]], [line_segment.p1[1], line_segment.p2[1]], color=c, linestyle='-')
+            lines.append(Line2D([0], [0], label='LineSegment_' + str(i), color=c))
+            i += 1
+
+        i = 0
+        for point in points:
+            c = next(color)
+            plt.plot(point[0], point[1], color=c, marker='o')
+            lines.append(Line2D([0], [0], marker='o', label='Point_' + str(i), color=c))
+            i += 1
+
+        plt.legend(handles=lines)
+        plt.show()
+
+
 class Signal:
     def __init__(self, signal):
         self.signal = signal.copy()
@@ -105,3 +186,7 @@ class Signal:
 
     def array(self):
         return self.signal
+
+def triangle_area(t):
+    """Calculates the area of a triangle defined given its 3 vertices. n_vertices x n_dims =  3 x 2"""
+    return (1 / 2) * abs((t[0][0] - t[2][0]) * (t[1][1] - t[0][1]) - (t[0][0] - t[1][0]) * (t[2][1] - t[0][1]))
