0272f4ca0493d784ab6643bef5a079821bfb60a6
Iason Sarantopoulos <iason@sarantop.com>
2020-04-13 12:32:40 +0300

    Add training script for dream PC

---
+++ experiments/2020.01.19_ddpg/test_ddpg_dream.py
@@ -0,0 +1,17 @@
+from robamine.algo.core import TrainWorld
+# from robamine.algo.ddpg_torch import DDPG_TORCH
+from robamine.algo.splitddpg import SplitDDPG
+from robamine import rb_logging
+import logging
+import yaml
+
+def run():
+    with open('params_dream.yml', 'r') as stream:
+        params = yaml.safe_load(stream)
+
+    rb_logging.init(directory=params['world']['logging_dir'], friendly_name=params['world']['friendly_name'], file_level=logging.INFO)
+    trainer = TrainWorld(agent=params['agent'], env=params['env'], params=params['world']['params'])
+    trainer.run()
+
+if __name__ == '__main__':
+    run()
