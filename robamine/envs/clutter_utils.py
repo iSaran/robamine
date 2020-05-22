fee76f07f293ea415a75cff219b90950ce7a50d1
Iason Sarantopoulos <iason@sarantop.com>
2020-05-22 14:47:26 +0300

    [clutter-utils] Add functionality for calculating point clouds

--- robamine/envs/clutter_utils.py
+++ robamine/envs/clutter_utils.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 from robamine.utils.math import LineSegment2D, triangle_area, min_max_scale
-from robamine.utils.orientation import rot_x, rot_z, rot2angleaxis
+from robamine.utils.orientation import rot_x, rot_z, rot2angleaxis, rot_y
 from robamine.utils.cv_tools import Feature
 from robamine.utils.info import get_now_timestamp
 
@@ -22,6 +22,7 @@ import matplotlib.patches as patches
 import os
 from robamine.algo.core import InvalidEnvError
 
+from scipy.spatial.distance import cdist
 
 from robamine.algo.util import Transition
 
@@ -520,3 +521,99 @@ def get_rotated_transition(transition, angle=0):
                       reward=transition.reward,
                       next_state=next_state,
                       terminal=transition.terminal)
+
+
+def get_distance_of_two_bbox(pose_1, bbox_1, pose_2, bbox_2, density=0.005):
+    '''Calculates the distance between two oriented bounding boxes using point clouds.'''
+    point_cloud_1 = discretize_3d_box(bbox_1[0], bbox_1[1], bbox_1[2], density)
+    point_cloud_2 = discretize_3d_box(bbox_2[0], bbox_2[1], bbox_2[2], density)
+
+    point_cloud_1 = transform_list_of_points(point_cloud_1, pose_1[0:3],
+                                              Quaternion(pose_1[3], pose_1[4], pose_1[5], pose_1[6]))
+    point_cloud_2 = transform_list_of_points(point_cloud_2, pose_2[0:3],
+                                              Quaternion(pose_2[3], pose_2[4], pose_2[5], pose_2[6]))
+
+    # Uncomment to plot point clouds
+    # import matplotlib.pyplot as plt
+    # from mpl_toolkits.mplot3d import Axes3D
+    # fig = plt.figure()
+    # ax = Axes3D(fig)
+    # ax.scatter(p[:, 0], p[:, 1], p[:, 2], marker='o')
+    # ax.axis('equal')
+    # plt.show()
+
+    return np.min(cdist(point_cloud_1, point_cloud_2))
+
+
+def transform_list_of_points(points, pos, quat, inv=False):
+    '''Points are w.r.t. {A}. pos and quat is the frame {A} w.r.t {B}. Returns the list of points experssed w.r.t.
+    {B}.'''
+    assert points.shape[1] == 3
+    matrix = np.eye(4)
+    matrix[0:3, 3] = pos
+    matrix[0:3, 0:3] = quat.rotation_matrix()
+    if inv:
+        matrix = np.linalg.inv(matrix)
+
+    transformed_points = np.transpose(np.matmul(matrix, np.transpose(
+        np.concatenate((points, np.ones((points.shape[0], 1))), axis=1))))[:, :3]
+    return transformed_points
+
+def discretize_2d_box(x, y, density):
+    assert x > 0 and y > 0
+
+    xx = np.linspace(-x, x, int(2 * x / density))
+    yy = np.linspace(-y, y, int(2 * y / density))
+    xx, yy = np.meshgrid(xx, yy)
+    out = np.zeros((int(2 * x / density) * int(2 * y / density), 3))
+    out[:, 0] = xx.flatten()
+    out[:, 1] = yy.flatten()
+    return out
+
+def discretize_3d_box(x, y, z, density):
+    combos = [[x, y, z, ''],
+              [x, y, -z, ''],
+              [z, y, -x, 'y',],
+              [z, y, x, 'y'],
+              [x, z, y, 'x'],
+              [x, z, -y, 'x']]
+    faces = []
+    for combo in combos:
+        face = discretize_2d_box(combo[0], combo[1], density)
+        face[:, 2] = combo[2]
+        if combo[3] == 'y':
+            rot = rot_y(pi / 2)
+            face = np.transpose(np.matmul(rot, np.transpose(face)))
+        elif combo[3] == 'x':
+            rot = rot_x(pi / 2)
+            face = np.transpose(np.matmul(rot, np.transpose(face)))
+        faces.append(face)
+    result = np.concatenate(faces, axis=0)
+    return result
+
+def plot_point_cloud_of_scene(obs_dict, density=0.005):
+    n_objects = int(obs_dict['n_objects'])
+    target_pose = obs_dict['object_poses'][0]
+    target_bbox = obs_dict['object_bounding_box'][0]
+    point_cloud_1 = discretize_3d_box(target_bbox[0], target_bbox[1], target_bbox[2], density)
+    point_cloud_1 = transform_list_of_points(point_cloud_1, target_pose[0:3],
+                                             Quaternion(target_pose[3], target_pose[4], target_pose[5], target_pose[6]))
+    point_clouds = []
+    point_clouds.append(point_cloud_1)
+    for i in range(1, n_objects):
+        obstacle_pose = obs_dict['object_poses'][i]
+        obstacle_bbox = obs_dict['object_bounding_box'][i]
+        point_cloud_2 = discretize_3d_box(obstacle_bbox[0], obstacle_bbox[1], obstacle_bbox[2], density)
+
+        point_cloud_2 = transform_list_of_points(point_cloud_2, obstacle_pose[0:3],
+                                                 Quaternion(obstacle_pose[3], obstacle_pose[4], obstacle_pose[5],
+                                                            obstacle_pose[6]))
+        point_clouds.append(point_cloud_2)
+    point_clouds = np.concatenate(point_clouds, axis=0)
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(point_clouds[:, 0], point_clouds[:, 1], point_clouds[:, 2], marker='o')
+    # ax.axis('equal')
+    plt.show()
