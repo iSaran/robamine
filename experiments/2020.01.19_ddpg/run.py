ef2d46c324b0a48fec79b59977191db78af8e152
Iason Sarantopoulos <iason@sarantop.com>
2020-07-06 12:52:45 +0300

    Revert "Push obstacle asymmetric experiment"
    This reverts commit 01041f716faaa16d06ce57638be012d24684df81.

--- experiments/2020.01.19_ddpg/run.py
+++ experiments/2020.01.19_ddpg/run.py
@@ -1049,7 +1049,7 @@ if __name__ == '__main__':
     # ----------
 
     # train(params)
-    train_eval(params)
+    # train_eval(params)
     # train_combo_q_learning(params)
     # eval_with_render(os.path.join(params['world']['logging_dir'], exp_dir))
     # eval_in_scenes(params, os.path.join(params['world']['logging_dir'], exp_dir), n_scenes=1000)
